7c37fe5fc0b90fca7308b5c103831eee7545e127
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2025-05-15 16:33:34 -0500

    added logic

--- app/lzip_app.cpp
+++ app/lzip_app.cpp
@@ -1,7 +1,6 @@
-#include "lzip/lzip.hpp"
-#include <iostream>
-
-int main(){
-  int result = lzip::add_one(1);
-  std::cout << "1 + 1 = " << result << std::endl;
+#include "lzip/cli.hpp"
+int main(int argc, char *argv[])
+{
+  lzw::runCLI(argc, argv);
+  return 0;
 }
