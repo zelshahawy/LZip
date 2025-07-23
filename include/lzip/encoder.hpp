7a2c460aaee33f88685680e44f9626853d1cc091
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2025-07-23 11:02:38 -0500

    fixed format

--- include/lzip/encoder.hpp
+++ include/lzip/encoder.hpp
@@ -1,71 +1,60 @@
 #pragma once
-#include <iostream>
-#include <vector>
-#include <string>
-#include "dictionary.hpp"
 #include "bitio.hpp"
+#include "dictionary.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
 
-namespace lzw
-{
+namespace lzw {
 
-	inline void execEncoding(std::istream &input, const std::string &filename)
-	{
-		auto [dict, lookup] = initDictionary();
-		int nextCode{256};
-		int codeSize{9};
-		constexpr int MAXBITS{20};
-		constexpr int MAXCODE{(1 << MAXBITS) - 1};
+inline void execEncoding(std::istream &input, const std::string &filename) {
+  auto [dict, lookup] = initDictionary();
+  int nextCode{256};
+  int codeSize{9};
 
-		auto bp = newBitPacker();
-		int p{-1};
-		std::vector<uint8_t> buf(4096);
+  constexpr int MAXBITS{20};
+  constexpr int MAXCODE{(1 << MAXBITS) - 1};
 
-		while (input)
-		{
-			input.read(reinterpret_cast<char *>(buf.data()), buf.size());
-			std::streamsize n = input.gcount();
-			for (std::streamsize i = 0; i < n; ++i)
-			{
-				uint8_t c{buf[i]};
-				DictKey key{p, int(c)};
-				auto it = lookup.find(key);
-				if (it != lookup.end())
-				{
-					p = it->second;
-				}
-				else
-				{
-					if (p != -1)
-						bp.writeCode(p, codeSize);
-					if (nextCode < MAXCODE)
-					{
-						dict.push_back(DictionaryEntry{p, c});
-						lookup[key] = nextCode;
-						++nextCode;
-						if (nextCode == (1 << codeSize) && codeSize < MAXBITS)
-							++codeSize;
-					}
-					p = c;
-				}
-			}
-		}
+  auto bp = newBitPacker();
+  int p{-1};
+  std::vector<uint8_t> buf(4096);
 
-		if (p != -1)
-			bp.writeCode(p, codeSize);
-		bp.flushRemaining();
-		bp.writeOutputToFile(filename + ".lzw");
-	}
+  while (input) {
+    input.read(reinterpret_cast<char *>(buf.data()), buf.size());
+    std::streamsize n = input.gcount();
+    for (std::streamsize i = 0; i < n; ++i) {
+      uint8_t c{buf[i]};
+      DictKey key{p, int(c)};
+      auto it = lookup.find(key);
+      if (it != lookup.end()) {
+        p = it->second;
+      } else {
+        if (p != -1)
+          bp.writeCode(p, codeSize);
+        if (nextCode < MAXCODE) {
+          dict.push_back(DictionaryEntry{p, c});
+          lookup[key] = nextCode;
+          ++nextCode;
+          if (nextCode == (1 << codeSize) && codeSize < MAXBITS)
+            ++codeSize;
+        }
+        p = c;
+      }
+    }
+  }
 
-	inline void startEncoding(std::istream &input, const std::string &filename)
-	{
-		try
-		{
-			execEncoding(input, filename);
-		}
-		catch (...)
-		{
-			std::cerr << "Error Encoding\n";
-		}
-	}
+  if (p != -1)
+    bp.writeCode(p, codeSize);
+  bp.flushRemaining();
+  bp.writeOutputToFile(filename + ".lzw");
+}
+
+inline void startEncoding(std::istream &input, const std::string &filename) {
+  try {
+    execEncoding(input, filename);
+  } catch (...) {
+    std::cerr << "Error Encoding\n";
+  }
+}
 
 } // namespace lzw
